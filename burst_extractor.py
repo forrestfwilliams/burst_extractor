c1865bd60d7c822bd0b0482d23ce3dba067e0fd4
Forrest Williams <ffwilliams2@alaska.edu>
2022-10-20 15:12:08 -0500

    stable burst extractor

--- burst_extractor.py
+++ burst_extractor.py
@@ -1,10 +1,11 @@
 import io
 import struct
+import xml.etree.ElementTree as ET
 import zipfile
 import zlib
-from IPython import embed
 
 import boto3
+from osgeo import gdal
 
 s3 = boto3.client('s3')
 
@@ -93,18 +94,104 @@ def extract_file(bucket, key, cd_start, filename):
     return content
 
 
+def extract_xml(bucket, key, cd_start, filename):
+    content = extract_file(bucket, key, cd_start, filename)
+    xml = ET.parse(io.BytesIO(content)).getroot()
+    return xml
+
+
+class BurstMetadata:
+    def __init__(self, annotation: ET.Element, burst_number):
+        self.annotation = annotation
+        self.burst_number = burst_number
+
+        self.burst = self.annotation.findall('.//{*}burst')[self.burst_number]
+        self.lines = int(self.annotation.findtext('.//{*}linesPerBurst'))
+        self.samples = int(self.annotation.findtext('.//{*}samplesPerBurst'))
+
+        firstValidSample = [int(val) for val in self.annotation.find('.//{*}firstValidSample').text.split()]
+        lastValidSample = [int(val) for val in self.annotation.find('.//{*}lastValidSample').text.split()]
+        first = False
+        last = False
+        for ii, val in enumerate(firstValidSample):
+            if (val >= 0) and (not first):
+                first = True
+                self.firstValidLine = ii
+
+            if (val < 0) and (first) and (not last):
+                last = True
+                self.numValidLines = ii - self.firstValidLine
+
+        self.lastValidLine = self.firstValidLine + self.numValidLines - 1
+
+        self.firstValidSample = max(firstValidSample[self.firstValidLine], firstValidSample[self.lastValidLine])
+        self.lastValidSample = min(lastValidSample[self.firstValidLine], lastValidSample[self.lastValidLine])
+
+        self.numValidSamples = self.lastValidSample - self.firstValidSample
+
+
+def createBurstVRT(swath_path, out_path, burst):
+    '''
+    Create a VRT file representing a single burst.
+    '''
+    src = gdal.Open(swath_path, gdal.GA_ReadOnly)
+    fullWidth = src.RasterXSize
+    fullLength = src.RasterYSize
+    del src
+    yoffset = (burst.burst_number) * burst.lines
+
+    rdict = {
+        'inwidth': burst.lastValidSample - burst.firstValidSample,
+        'inlength': burst.lastValidLine - burst.firstValidLine,
+        'outwidth': burst.samples,
+        'outlength': burst.lines,
+        'filename': swath_path,
+        'yoffset': yoffset + burst.firstValidLine,
+        'localyoffset': burst.firstValidLine,
+        'xoffset': burst.firstValidSample,
+        'fullwidth': fullWidth,
+        'fulllength': fullLength,
+    }
+
+    tmpl = '''<VRTDataset rasterXSize="{outwidth}" rasterYSize="{outlength}">
+    <VRTRasterBand dataType="CFloat32" band="1">
+        <NoDataValue>0.0</NoDataValue>
+        <SimpleSource>
+            <SourceFilename relativeToVRT="1">{filename}</SourceFilename>
+            <SourceBand>1</SourceBand>
+            <SourceProperties RasterXSize="{fullwidth}" RasterYSize="{fulllength}" DataType="CInt16"/>
+            <SrcRect xOff="{xoffset}" yOff="{yoffset}" xSize="{inwidth}" ySize="{inlength}"/>
+            <DstRect xOff="{xoffset}" yOff="{localyoffset}" xSize="{inwidth}" ySize="{inlength}"/>
+        </SimpleSource>
+    </VRTRasterBand>
+</VRTDataset>'''
+
+    with open(out_path, 'w') as f:
+        f.write(tmpl.format(**rdict))
+
+
 if __name__ == '__main__':
     # import cProfile
     # import pstats
 
     bucket = 'ffwilliams2-shenanigans'
     data = 'bursts/S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.zip'
-    filename = 'S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.SAFE/measurement/s1a-iw1-slc-vh-20200604t022252-20200604t022317-032861-03ce65-001.tiff'
+    swath_path = 'S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.SAFE/measurement/s1a-iw2-slc-vv-20200604t022253-20200604t022318-032861-03ce65-005.tiff'
+    annotation_path = 'S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.SAFE/annotation/s1a-iw2-slc-vv-20200604t022253-20200604t022318-032861-03ce65-005.xml'
+    burst_number = 7
     # with cProfile.Profile() as pr:
     zip_file, cd_start = get_zip_file(bucket, data)
-    swath = extract_file(bucket, data, cd_start, filename)
-    with open('test.tif', 'wb') as f:
-        f.write(data)
+    annotation = extract_xml(bucket, data, cd_start, annotation_path)
+    burst = BurstMetadata(annotation, burst_number)
+
+    swath_bytes = extract_file(bucket, data, cd_start, swath_path)
+    with open('swath.tif', 'wb') as f:
+        f.write(swath_bytes)
+
+    createBurstVRT('swath.tif', f'burst_0{burst_number+1}.vrt', burst)
+    src = gdal.Open(f'burst_0{burst_number+1}.vrt')
+    src = gdal.Translate(f'burst_0{burst_number+1}.tif', src, format='GTiff')
+    del src
 
     # stats = pstats.Stats(pr)
     # stats.sort_stats(pstats.SortKey.TIME)
