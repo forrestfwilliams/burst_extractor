231865196c76f5e3063908db494c704d1cee3e47
Forrest Williams <ffwilliams2@alaska.edu>
2022-10-21 21:12:16 +0000

    work async download

--- benchmark.py
+++ benchmark.py
@@ -3,30 +3,32 @@ import math
 import time
 
 from aiobotocore.session import get_session
+from botocore.config import Config
 import boto3
 
+KB = 1024
+MB = KB * KB
 
-def get_chunks(start, length, chunk_size=10 * 1024):
+def get_chunks(start, length, chunk_size):
     n_chunks = math.floor(length / chunk_size)
-    n_chunks = min(n_chunks, 3)
     starts = [start + chunk_size * i for i in range(n_chunks)] + [chunk_size * n_chunks]
     stops = [(start + chunk_size * (i + 1)) - 1 for i in range(n_chunks)] + [length]
     return starts, stops
 
 
-async def get_async(bucket, key, start, stop):
-    session = get_session()
-    async with session.create_client('s3', region_name='us-west-2') as client:
-        resp = await client.get_object(Bucket=bucket, Key=key, Range=f'bytes={start}-{stop}')
-        async with resp['Body'] as stream:
-            body = await stream.read()
+async def get_async(client, bucket, key, start, stop):
+    resp = await client.get_object(Bucket=bucket, Key=key, Range=f'bytes={start}-{stop}')
+    body = await resp['Body'].read()
     return body
 
 
-async def download_range_async(bucket, key, start, length, chunk_size=10 * 1024):
+async def download_range_async(bucket, key, start, length, chunk_size):
     starts, stops = get_chunks(start, length, chunk_size)
-    jobs = [get_async(bucket, key, i, j) for i, j in zip(starts, stops)]
-    byte_list = await asyncio.gather(*jobs)
+    session = get_session()
+    config = Config(max_pool_connections=100)
+    async with session.create_client('s3', region_name='us-west-2', config=config) as client:
+        jobs = [get_async(client, bucket, key, i, j) for i, j in zip(starts, stops)]
+        byte_list = await asyncio.gather(*jobs)
     return b''.join(byte_list)
 
 
@@ -54,13 +56,12 @@ if __name__ == '__main__':
     # end = time.time()
     # print(f'Multipart downloaded in {end-start:.2f} seconds')
 
-    # Async GET (completes in 39s)
+    # Async GET (Completes in 16s)
     start = time.time()
     s3 = boto3.client('s3')
     content_length = s3.get_object(Bucket=bucket, Key=key)['ContentLength']
-    breakpoint()
-    content = asyncio.run(download_range_async(bucket, key, 0, content_length))
+    content = asyncio.run(download_range_async(bucket, key, 0, content_length, chunk_size=25*MB))
     with open('async.tif', 'wb') as f:
         f.write(content)
     end = time.time()
-    print(f'Get downloaded in {end-start:.2f} seconds')
+    print(f'Async downloaded in {end-start:.2f} seconds')
