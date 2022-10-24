ac9b7b0cfe56cb4ef9bf6a9322593e984e0d127a
Forrest Williams <ffwilliams2@alaska.edu>
2022-10-24 15:12:04 -0500

    burst_extractor: refactor to use S3Zip class

--- burst_extractor.py
+++ burst_extractor.py
@@ -4,55 +4,31 @@ import struct
 import tempfile
 import xml.etree.ElementTree as ET
 import zipfile
-from isal import isal_zlib
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 import boto3
+from isal import isal_zlib
 from osgeo import gdal
 
-EOCD_RECORD_SIZE = 22
-ZIP64_EOCD_RECORD_SIZE = 56
-ZIP64_EOCD_LOCATOR_SIZE = 20
-MAX_STANDARD_ZIP_SIZE = 4_294_967_295
 KB = 1024
 MB = KB * KB
 MULTIPART_THRESHOLD = 8 * MB
 MULTIPART_CHUNKSIZE = 8 * MB
+EOCD_RECORD_SIZE = 22
+ZIP64_EOCD_RECORD_SIZE = 56
+ZIP64_EOCD_LOCATOR_SIZE = 20
+MAX_STANDARD_ZIP_SIZE = 4_294_967_295
 ZLIB_MAX_WBITS = 15
 
 
-def get_zip_file(s3_client, bucket, key):
-    file_size = get_file_size(s3_client, bucket, key)
-    eocd_record = fetch(s3_client, bucket, key, file_size - EOCD_RECORD_SIZE, EOCD_RECORD_SIZE)
-    if file_size <= MAX_STANDARD_ZIP_SIZE:
-        print('accessing zip')
-        cd_start, cd_size = get_central_directory_metadata_from_eocd(eocd_record)
-        central_directory = fetch(s3_client, bucket, key, cd_start, cd_size)
-        return zipfile.ZipFile(io.BytesIO(central_directory + eocd_record)), cd_start
-    else:
-        print('accessing zip64')
-        zip64_eocd_record = fetch(
-            s3_client,
-            bucket,
-            key,
-            file_size - (EOCD_RECORD_SIZE + ZIP64_EOCD_LOCATOR_SIZE + ZIP64_EOCD_RECORD_SIZE),
-            ZIP64_EOCD_RECORD_SIZE,
-        )
-        zip64_eocd_locator = fetch(
-            s3_client, bucket, key, file_size - (EOCD_RECORD_SIZE + ZIP64_EOCD_LOCATOR_SIZE), ZIP64_EOCD_LOCATOR_SIZE
-        )
-        cd_start, cd_size = get_central_directory_metadata_from_eocd64(zip64_eocd_record)
-        central_directory = fetch(s3_client, bucket, key, cd_start, cd_size)
-        return (
-            zipfile.ZipFile(io.BytesIO(central_directory + zip64_eocd_record + zip64_eocd_locator + eocd_record)),
-            cd_start,
-        )
+def parse_short(in_bytes):
+    return ord(in_bytes[0:1]) + (ord(in_bytes[1:2]) << 8)
 
 
-def get_file_size(s3_client, bucket, key):
-    head_response = s3_client.head_object(Bucket=bucket, Key=key)
-    return head_response['ContentLength']
+def bytes_to_xml(in_bytes):
+    xml = ET.parse(io.BytesIO(in_bytes)).getroot()
+    return xml
 
 
 def calculate_range_parameters(total_size, offset, chunk_size):
@@ -68,80 +44,95 @@ def calculate_range_parameters(total_size, offset, chunk_size):
     return range_params
 
 
-def threaded_s3_get(args):
-    s3_client, bucket, key, range_header = args
-    resp = s3_client.get_object(Bucket=bucket, Key=key, Range=range_header)
-    body = resp['Body'].read()
-    return body
+class S3Zip:
+    def __init__(self, s3_client, bucket, key):
+        self.client = s3_client
+        self.bucket = bucket
+        self.key = key
+        self.zip_dir, self.cd_start = self.get_zip_dir()
 
+    def parse_little_endian_to_int(little_endian_bytes):
+        format_character = "i" if len(little_endian_bytes) == 4 else "q"
+        return struct.unpack("<" + format_character, little_endian_bytes)[0]
 
-def threaded_s3_get_workflow(s3_client, bucket, key, offset, file_size, chunk_size):
-    # Define some work to be done, this can be anything
+    def get_central_directory_metadata_from_eocd(self, eocd):
+        cd_size = self.parse_little_endian_to_int(eocd[12:16])
+        cd_start = self.parse_little_endian_to_int(eocd[16:20])
+        return cd_start, cd_size
 
-    my_tasks = [[s3_client, bucket, key, i] for i in calculate_range_parameters(file_size, offset, chunk_size)]
+    def get_central_directory_metadata_from_eocd64(self, eocd64):
+        cd_size = self.parse_little_endian_to_int(eocd64[40:48])
+        cd_start = self.parse_little_endian_to_int(eocd64[48:56])
+        return cd_start, cd_size
 
-    # Dispatch work tasks with our s3_client
-    with ThreadPoolExecutor(max_workers=20) as executor:
-        results = executor.map(threaded_s3_get, my_tasks)
+    def print_zip_content(self):
+        files = [zi.filename for zi in self.zip_dir.filelist]
+        print(f"Files: {files}")
 
-    content = b''.join(results)
-    return content
+    def get_file_size(self):
+        head_response = self.client.head_object(Bucket=self.bucket, Key=self.key)
+        return head_response['ContentLength']
 
+    def ranged_s3_get(self, range_header):
+        resp = self.client.get_object(Bucket=self.bucket, Key=self.key, Range=range_header)
+        body = resp['Body'].read()
+        return body
 
-def fetch(s3_client, bucket, key, start, length):
-    if length <= MULTIPART_THRESHOLD:
-        end = start + length - 1
-        response = s3_client.get_object(Bucket=bucket, Key=key, Range="bytes=%d-%d" % (start, end))
-        content = response['Body'].read()
-    else:
-        content = threaded_s3_get_workflow(s3_client, bucket, key, start, length, MULTIPART_CHUNKSIZE)
-    return content
+    def threaded_s3_get(self, offset, file_size, chunk_size):
+        range_params = calculate_range_parameters(file_size, offset, chunk_size)
 
+        # Dispatch work tasks with our s3_client
+        with ThreadPoolExecutor(max_workers=20) as executor:
+            results = executor.map(self.ranged_s3_get, range_params)
 
-def get_central_directory_metadata_from_eocd(eocd):
-    cd_size = parse_little_endian_to_int(eocd[12:16])
-    cd_start = parse_little_endian_to_int(eocd[16:20])
-    return cd_start, cd_size
+        content = b''.join(results)
+        return content
 
+    def get(self, start, length, threshold=MULTIPART_THRESHOLD, chunk_size=MULTIPART_CHUNKSIZE):
+        if length <= threshold:
+            end = start + length - 1
+            content = self.ranged_s3_get(f'bytes={start}-{end}')
+        else:
+            content = self.threaded_s3_get(start, length, chunk_size)
+        return content
 
-def get_central_directory_metadata_from_eocd64(eocd64):
-    cd_size = parse_little_endian_to_int(eocd64[40:48])
-    cd_start = parse_little_endian_to_int(eocd64[48:56])
-    return cd_start, cd_size
+    def get_zip_dir(self):
+        file_size = self.get_file_size()
+        eocd_record = self.get(file_size - EOCD_RECORD_SIZE, EOCD_RECORD_SIZE)
+        if file_size <= MAX_STANDARD_ZIP_SIZE:
+            print('accessing zip')
+            cd_start, cd_size = self.get_central_directory_metadata_from_eocd(eocd_record)
+            central_directory = self.get(cd_start, cd_size)
+            return zipfile.ZipFile(io.BytesIO(central_directory + eocd_record)), cd_start
+        else:
+            print('accessing zip64')
+            zip64_eocd_record = self.get(
+                file_size - (EOCD_RECORD_SIZE + ZIP64_EOCD_LOCATOR_SIZE + ZIP64_EOCD_RECORD_SIZE),
+                ZIP64_EOCD_RECORD_SIZE,
+            )
+            zip64_eocd_locator = self.get(
+                file_size - (EOCD_RECORD_SIZE + ZIP64_EOCD_LOCATOR_SIZE),
+                ZIP64_EOCD_LOCATOR_SIZE,
+            )
+            cd_start, cd_size = self.get_central_directory_metadata_from_eocd64(zip64_eocd_record)
+            central_directory = self.get(cd_start, cd_size)
+            return (
+                zipfile.ZipFile(io.BytesIO(central_directory + zip64_eocd_record + zip64_eocd_locator + eocd_record)),
+                cd_start,
+            )
 
+    def extract_file(self, filename):
+        zi = [zi for zi in self.zip_dir.filelist if zi.filename == filename][0]
+        file_head = self.get(self.cd_start + zi.header_offset + 26, 4)
+        name_len = parse_short(file_head[0:2])
+        extra_len = parse_short(file_head[2:4])
 
-def parse_little_endian_to_int(little_endian_bytes):
-    format_character = "i" if len(little_endian_bytes) == 4 else "q"
-    return struct.unpack("<" + format_character, little_endian_bytes)[0]
+        content_offset = self.cd_start + zi.header_offset + 30 + name_len + extra_len
+        content = self.get(content_offset, zi.compress_size)
+        if zi.compress_type == zipfile.ZIP_DEFLATED:
+            content = isal_zlib.decompressobj(-15).decompress(content)
 
-
-def print_zip_content(zip_file):
-    files = [zi.filename for zi in zip_file.filelist]
-    print(f"Files: {files}")
-
-
-def parse_short(in_bytes):
-    return ord(in_bytes[0:1]) + (ord(in_bytes[1:2]) << 8)
-
-
-def extract_file(s3_client, bucket, key, cd_start, filename):
-    zi = [zi for zi in zip_file.filelist if zi.filename == filename][0]
-    file_head = fetch(s3_client, bucket, key, cd_start + zi.header_offset + 26, 4)
-    name_len = parse_short(file_head[0:2])
-    extra_len = parse_short(file_head[2:4])
-
-    content_offset = cd_start + zi.header_offset + 30 + name_len + extra_len
-    content = fetch(s3_client, bucket, key, content_offset, zi.compress_size)
-    if zi.compress_type == zipfile.ZIP_DEFLATED:
-        content = isal_zlib.decompressobj(-15).decompress(content)
-
-    return content
-
-
-def extract_xml(s3_client, bucket, key, cd_start, filename):
-    content = extract_file(s3_client, bucket, key, cd_start, filename)
-    xml = ET.parse(io.BytesIO(content)).getroot()
-    return xml
+        return content
 
 
 class BurstMetadata:
@@ -241,13 +232,13 @@ if __name__ == '__main__':
     s3 = boto3.client('s3')
 
     bucket = 'ffwilliams2-shenanigans'
-    data = 'bursts/S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.zip'
+    key = 'bursts/S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.zip'
     swath_path = 'S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.SAFE/measurement/s1a-iw2-slc-vv-20200604t022253-20200604t022318-032861-03ce65-005.tiff'
     annotation_path = 'S1A_IW_SLC__1SDV_20200604T022251_20200604T022318_032861_03CE65_7C85.SAFE/annotation/s1a-iw2-slc-vv-20200604t022253-20200604t022318-032861-03ce65-005.xml'
 
-    zip_file, cd_start = get_zip_file(s3, bucket, data)
-    annotation = extract_xml(s3, bucket, data, cd_start, annotation_path)
-    swath_bytes = extract_file(s3, bucket, data, cd_start, swath_path)
+    safe_zip = S3Zip(s3, bucket, key)
+    annotation = bytes_to_xml(safe_zip.extract_file(annotation_path))
+    swath_bytes = safe_zip.extract_file(swath_path)
     with open('swath.tif', 'wb') as f:
         f.write(swath_bytes)
 
